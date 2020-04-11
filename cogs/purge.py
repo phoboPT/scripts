00c8f97d427fc954e186b4a74f61447b62c4402e
phoboPT <hugo.branco.c@gmail.com>
2020-04-11 21:35:26 +0100

    asd

--- cogs/purge.py
+++ cogs/purge.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 
 def isAdmin(ctx):
-    return ctx.author.id == 343714644147568650 or ctx.author.id == 686986610142740521 or ctx.author.id == 55841874546340659 or ctx.author.id == 619574286356578336 or ctx.author.id == 661953774734016512
+    return ctx.author.id == 343714644147568650 or ctx.author.id == 686986610142740521 or ctx.author.id == 558418745463406594 or ctx.author.id == 619574286356578336 or ctx.author.id == 661953774734016512
 
 
 class Purge(commands.Cog):
