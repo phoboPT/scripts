345491e2ecee349eb768fa2f6c39e8ecf24e0ae5
phoboPT <hugo.branco.c@gmail.com>
2020-06-07 14:51:35 +0100

    dfgdf

--- discordBot.py
+++ discordBot.py
@@ -8,9 +8,15 @@ load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 bot = commands.Bot(command_prefix='!')
 
-for filename in os.listdir('./cogs'):
-    if (filename.endswith('.py')):
-        bot.load_extension(f'cogs.{filename[:-3]}')
+
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f'cogs.{extension}')
+
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
 
 
 @bot.event
@@ -78,6 +84,10 @@ async def levelUp(users, user, channel, message):
     if (lvlStart < lvlEnd):
         await message.channel.send('{} reached a new level {} 🎉'.format(user.mention, lvlEnd))
         users[str(user.id)]['level'] = lvlEnd
+
+for filename in os.listdir('./cogs'):
+    if (filename.endswith('.py')):
+        bot.load_extension(f'cogs.{filename[:-3]}')
 
 
 bot.run(DISCORD_TOKEN)
