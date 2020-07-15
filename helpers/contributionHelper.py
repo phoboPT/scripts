de20a1c5cbd2352b99991026a5381919eb2be74d
phoboPT <hugo.branco.c@gmail.com>
2020-07-15 14:13:47 +0100

    sdsd

--- helpers/contributionHelper.py
+++ helpers/contributionHelper.py
@@ -93,106 +93,61 @@ async def getOne(args):
             mycursor.execute(companyFlightsYesterdaySQL)
             companyFlightsYesterday = mycursor.fetchall()
 
-            # # Graph
-            # # day1
-            # index = f'R{row}'
-            # day1 = wks.acell(index).value
-            # # day2
-            # index = f'S{row}'
-            # day2 = wks.acell(index).value
-            # # day3
-            # index = f'T{row}'
-            # day3 = wks.acell(index).value
-            # # day4
-            # index = f'U{row}'
-            # day4 = wks.acell(index).value
-            # # day5
-            # index = f'V{row}'
-            # day5 = wks.acell(index).value
-            # # day6
-            # index = f'W{row}'
-            # day6 = wks.acell(index).value
-            # # day7
-            # index = f'X{row}'
-            # day7 = wks.acell(index).value
-            # # yesterday
-            # index = f'N{row}'
-            # day8 = wks.acell(index).value
+            days = [0,  0,  0, 0,  0,  0,  0,  0]
+            i = 0
+            for x in days:
+                i += 1
+                yesterdayDate = now - timedelta(i)
+                companyContrtibutionSQL = f"SELECT * FROM contribution WHERE companyID = {companyID} AND  data between'{yesterdayDate.year}-{yesterdayDate.month}-{yesterdayDate.day} 00:00' AND '{yesterdayDate.year}-{yesterdayDate.month}-{yesterdayDate.day} 23:59'"
+                mycursor.execute(companyContrtibutionSQL)
+                companyContribution = mycursor.fetchall()
+                if (len(companyContribution) < 1):
+                    break
+                days[len(days)-1] = companyContribution[len(
+                    companyContribution) - 1][2] - companyContribution[0][2]
 
-            # day1 = int(re.sub('[,]+', '', day1))
-            # day2 = int(re.sub('[,]+', '', day2))
-            # day3 = int(re.sub('[,]+', '', day3))
-            # day4 = int(re.sub('[,]+', '', day4))
-            # day5 = int(re.sub('[,]+', '', day5))
-            # day6 = int(re.sub('[,]+', '', day6))
-            # day7 = int(re.sub('[,]+', '', day7))
-            # day8 = int(re.sub('[$,]+', '', day8))
+            total = 0
+            for x in days:
+                total = total + x
+            plt.clf()
+            names = ["1", "2", "3", "4", "5", "6", "7", "Yesterday"]
+            yAxys = sorted(days)
 
-            # total = day2+day3+day4+day5+day6+day7+day8
-            # plt.clf()
+            last = yAxys[7] + 2000
 
-            # days = {"1": day1, "2": day2, "3": day3,
-            #         "4": day4, "5": day5, "6": day6, "7": day7, "Yesterday": day8}
+            i = 0
+            for x in yAxys:
+                yAxys[i] = f'{locale.format_string("%d", yAxys[i],grouping=True)}'
+                i += 1
+            yAxys.insert(
+                8, f'{locale.format_string("%d", last,grouping=True)}')
 
-            # names = list(days.keys())
-            # values = list(days.values())
-            # yAxys = sorted(values)
+            i = 0
+            for x in days:
+                days[i] = f'{locale.format_string("%d", days[i],grouping=True)}'
+                i += 1
 
-            # last = yAxys[7] + 2000
-            # yAxys[0] = f'{locale.format_string("%d", yAxys[0],grouping=True)}'
-            # yAxys[1] = f'{locale.format_string("%d", yAxys[1],grouping=True)}'
-            # yAxys[2] = f'{locale.format_string("%d", yAxys[2],grouping=True)}'
-            # yAxys[3] = f'{locale.format_string("%d", yAxys[3],grouping=True)}'
-            # yAxys[4] = f'{locale.format_string("%d", yAxys[4],grouping=True)}'
-            # yAxys[5] = f'{locale.format_string("%d", yAxys[5],grouping=True)}'
-            # yAxys[6] = f'{locale.format_string("%d", yAxys[6],grouping=True)}'
-            # yAxys[7] = f'{locale.format_string("%d", yAxys[7],grouping=True)}'
-            # yAxys.insert(
-            #     8, f'{locale.format_string("%d", last,grouping=True)}')
+            fig = plt.figure()
 
-            # values[0] = f'{locale.format_string("%d", values[0],grouping=True)}'
-            # values[1] = f'{locale.format_string("%d", values[1],grouping=True)}'
-            # values[2] = f'{locale.format_string("%d", values[2],grouping=True)}'
-            # values[3] = f'{locale.format_string("%d", values[3],grouping=True)}'
-            # values[4] = f'{locale.format_string("%d", values[4],grouping=True)}'
-            # values[5] = f'{locale.format_string("%d", values[5],grouping=True)}'
-            # values[6] = f'{locale.format_string("%d", values[6],grouping=True)}'
-            # values[7] = f'{locale.format_string("%d", values[7],grouping=True)}'
+            plt.style.use('dark_background')
+            ax = plt.axes()
 
-            # fig = plt.figure()
+            ax.plot([0, 0, 0, 0, 0, 0, 0, 0, 0], yAxys, color='black')
+            # zip joins x and y co  ordinates in pairs
+            for y, z in zip(names, days):
+                label = z
+                plt.annotate(label,  # this is the text
+                             (y, z),  # this is the point to label
+                             textcoords="offset points",  # how to position the text
+                             # distance from text to points (x,y)
+                             xytext=(0, 10),
+                             ha='center')  # horizontal alignment can be left, right or center
 
-            # plt.style.use('dark_background')
-            # ax = plt.axes()
+            plt.plot(names, days, 'o-', label='curPerform', color='r')
+            # plt.show()
+            plt.savefig("online.png")
+            plt.close(fig=fig)
 
-            # ax.plot([0, 0, 0, 0, 0, 0, 0, 0, 0], yAxys, color='black')
-            # # zip joins x and y co  ordinates in pairs
-            # for y, z in zip(names, values):
-            #     label = z
-            #     plt.annotate(label,  # this is the text
-            #                  (y, z),  # this is the point to label
-            #                  textcoords="offset points",  # how to position the text
-            #                  # distance from text to points (x,y)
-            #                  xytext=(0, 10),
-            #                  ha='center')  # horizontal alignment can be left, right or center
-
-            # plt.plot(names, values, 'o-', label='curPerform', color='r')
-            # # plt.show()
-            # plt.savefig("online.png")
-            # plt.close(fig=fig)
-
-            # index = f'P{row}'
-            # yesterdayFlight = wks.acell(index).value
-            # yesterdayFlightValue = int(re.sub(',', '', yesterdayFlight))
-            # index = f'E{row}'
-            # todayCont = wks.acell(index).value
-            # todayContValue = int(re.sub('[$,]+', '', todayCont))
-
-            # index = f'F{row}'
-            # todayFlight = wks.acell(index).value
-            # todayFlightValue = int(re.sub('[$,]+', '', todayFlight))
-
-            # if (int(delta2.days) < int(delta)):
-            #     delta = delta2.days
             if (len(companyContribution) < 1):
                 return data
             contriDay = round(x["contributed"] / companyData[0][3], 2)
@@ -212,7 +167,7 @@ async def getOne(args):
             data['share'] = f'$ {companyShare[len(companyShare)-1][2]}'
             # data['place'] = f'$ {locale.format_string("%d", total, grouping=True)}'
             data['diffYesterday'] = f'$ {locale.format_string("%d", x["contributed"]-companyContributionYesterday[len(companyContributionYesterday)-1][2],grouping=True)}'
-            data['yesterday'] = f'{locale.format_string("%d",companyContributionYesterday[len(companyContributionYesterday)-1][2]-companyContributionYesterday[0][2] , grouping=True)}'
+            data['yesterday'] = f'$ {locale.format_string("%d",companyContributionYesterday[len(companyContributionYesterday)-1][2]-companyContributionYesterday[0][2] , grouping=True)}'
             data['flightYesterday'] = locale.format_string(
                 "%d", companyFlightsYesterday[len(companyFlightsYesterday)-1][2] - companyFlightsYesterday[0][2], grouping=True)
             data['flightDiff'] = f'{locale.format_string("%d", x["flights"]-companyFlightsYesterday[len(companyFlightsYesterday)-1][2],grouping=True)}'
